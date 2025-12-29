031383ca6a51d99589ecbf5d89014c969c7fcb4d
harshawilson1 <nadarharshaw@gmail.com>
2025-12-29 16:36:25 +0530

    change in app.py

--- app.py
+++ app.py
@@ -1229,6 +1229,6 @@ def update_profile():
 @app.route("/logout")
 def logout():
     session.clear()
-    return redirect("/")
+    return redirect("/login")
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
