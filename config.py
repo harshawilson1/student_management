cc0f22f41edc13cb3fe67a1ffdc0879b6de41bea
harsha Wilson <nadarharshaw@gmail.com>
2025-12-29 16:17:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
+import os
+
 MYSQL_CONFIG = {
-    "host": "localhost",
-    "user": "root",
-    "password": "root123",
-    "database": "student_management"
+    "host": os.environ.get("DB_HOST"),
+    "user": os.environ.get("DB_USER"),
+    "password": os.environ.get("DB_PASSWORD"),
+    "database": os.environ.get("DB_NAME"),
+    "port": int(os.environ.get("DB_PORT", 3306))
 }
